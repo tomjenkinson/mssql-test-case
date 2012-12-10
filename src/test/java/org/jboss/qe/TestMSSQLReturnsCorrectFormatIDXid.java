3ea5dc6212e5ef3c5e325890ff91f81de4696fa7
Tom Jenkinson <tom.jenkinson@redhat.com>
2012-12-10 19:55:29 +0000

    Added some business logic

--- src/test/java/org/jboss/qe/TestMSSQLReturnsCorrectFormatIDXid.java
+++ src/test/java/org/jboss/qe/TestMSSQLReturnsCorrectFormatIDXid.java
@@ -2,7 +2,9 @@ package org.jboss.qe;
 
 import static org.junit.Assert.assertTrue;
 
+import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Arrays;
 import java.util.Random;
 
@@ -13,6 +15,7 @@ import javax.transaction.xa.Xid;
 
 import org.junit.Test;
 
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
 import com.microsoft.sqlserver.jdbc.SQLServerXADataSource;
 
 /**
@@ -22,16 +25,55 @@ public class TestMSSQLReturnsCorrectFormatIDXid {
 
 	@Test
 	public void test() throws SQLException, XAException {
-		SQLServerXADataSource dataSource = new com.microsoft.sqlserver.jdbc.SQLServerXADataSource();
 
-		dataSource.setURL(System.getProperty("url"));
-		XAConnection xaConnection = dataSource.getXAConnection(System.getProperty("user"), System.getProperty("password"));
-		XAResource xaResource = xaConnection.getXAResource();
-		Xid xid = new XidImple();
-		xaResource.start(xid, 0);
-		xaResource.prepare(xid);
-		Xid[] recover = xaResource.recover(0);
-		assertTrue(Arrays.asList(recover).contains(xid));
+		{
+			SQLServerDataSource dataSource = new com.microsoft.sqlserver.jdbc.SQLServerDataSource();
+
+			dataSource.setURL(System.getProperty("url"));
+			Connection connection = dataSource.getConnection(
+					System.getProperty("user"), System.getProperty("password"));
+			Statement stmt = connection.createStatement();
+
+			try {
+				stmt.executeUpdate("DROP TABLE tomsTest");
+			} catch (SQLException ex) {
+				if (!ex.getSQLState().equals("S0005")
+						&& ex.getErrorCode() != 3701) {
+					throw ex;
+				}
+			}
+
+			String statement = "CREATE TABLE tomsTest (tomsInt INTEGER NOT NULL, PRIMARY KEY(tomsInt))";
+			stmt.executeUpdate(statement);
+			connection.close();
+		}
+
+		{
+			SQLServerXADataSource dataSource = new com.microsoft.sqlserver.jdbc.SQLServerXADataSource();
+
+			dataSource.setURL(System.getProperty("url"));
+			XAConnection xaConnection = dataSource.getXAConnection(
+					System.getProperty("user"), System.getProperty("password"));
+			XAResource xaResource = xaConnection.getXAResource();
+			Xid xid = new XidImple();
+			xaResource.start(xid, 0);
+			Statement stmt = xaConnection.getConnection().createStatement();
+			stmt.execute("INSERT INTO tomsTest VALUES (1)");
+			xaResource.prepare(xid);
+			Xid[] recover = xaResource.recover(0);
+			assertTrue(Arrays.asList(recover).contains(xid));
+			xaConnection.close();
+		}
+
+		{
+			SQLServerDataSource dataSource = new com.microsoft.sqlserver.jdbc.SQLServerDataSource();
+			dataSource.setURL(System.getProperty("url"));
+			Connection connection = dataSource.getConnection(
+					System.getProperty("user"), System.getProperty("password"));
+			Statement stmt = connection.createStatement();
+			stmt.executeUpdate("DROP TABLE tomsTest");
+			connection.close();
+		}
 	}
 
 	private class XidImple implements javax.transaction.xa.Xid {
